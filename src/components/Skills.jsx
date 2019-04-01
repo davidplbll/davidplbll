ff62a46c3ec0d31f8828fde2ca57cc72b92ef4c4
DAVID <davidplbll@gmail.com>
2019-04-01 10:44:22 -0500

    animaciones skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Styled, { ThemeProvider } from 'styled-components';
+import Styled, { keyframes  } from 'styled-components';
 
 const SkillsContainer=Styled.div`
   display:grid;
@@ -17,16 +17,36 @@ const SkillsLine=Styled.div`
   border-radius:50px;
   background:#F8BBD0;
 `;
-const SkillsSpan=Styled.span`
-  display:block;
-  height:100%;
-  width:${props=>props.width};
-  border-radius:8px;
-  position:relative;
-  background-color:#C2185B;
-  overflow:hidden;
-  box-shadow:inset 8 2px 9px rgba(255,255,255,0.3) inset 0 -2px 6px rgba(0,0,0,0.4)
+
+const move = keyframes`
+  0% { background-position: 0 0; }
+  100% { background-position: 50px 50px; }
 `;
+
+const SkillsSpan = Styled.span`
+  display: block;
+  height: 100%;
+  width: ${props => props.width};
+  border-radius: 8px;
+  background-color: #C2185B;
+  overflow: hidden;
+  position: relative;
+  box-shadow: inset 0 2px 9px rgba(255,255,255,0.3), inset 0 -2px 6px rgba(0,0,0,0.4);
+  &:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-image: linear-gradient(-45deg, rgba(255,255,255,0.2) 25%, transparent 25%, transparent 50%, rgba(255,255,255,0.2) 50%, rgba(255,255,255,0.2) 75%, transparent 75%, transparent);
+    z-index: 1;
+    background-size: 50px 50px;
+    animation: ${move} 4s linear infinite;
+    overflow: hidden;
+  }
+`;
+
 const Skills=props=>(
   <div className="Skills">
     <SkillsContainer>
